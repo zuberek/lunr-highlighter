095c0897b7799e4ee2a3aa0af8241b71640c81d0
Jan K Dabrowski <dabrowski.janek@gmail.com>
2019-03-26 10:31:03 +0800

    adding tests

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = function printMsg() {
+    return "This is a message from the demo package";
+  }
---
+++ tests/test.js
@@ -0,0 +1,4 @@
+const printMsg = require('../index');
+const assert = require('assert');
+
+assert.strictEqual('This is a message from the demo package', printMsg());
